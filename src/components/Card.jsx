ed3f8729dae294bc788af6ba1f51e9b760b65c50
Yasmin Silva <alunoyasminsilva07@gmail.com>
2024-05-27 17:47:53 +0000

    finalização do html

---
+++ src/components/Card.jsx
@@ -0,0 +1,21 @@
+export default function Card() {
+  return (
+    <div>
+        <main>
+            <img src="" alt="" />
+            <h2>Yasmin Silva</h2>
+            <h3>São Paulo, Brasil</h3>
+
+            <p>"Front-end developer and avid reader."</p>
+
+            <ul>
+                <li><a href="">GitHub</a></li>
+                <li><a href="">FrontEnd Mentor</a></li>
+                <li><a href="">LinkedIn</a></li>
+                <li><a href="">Twitter</a></li>
+                <li><a href="">Instagram</a></li>
+            </ul>
+        </main>
+    </div>
+  )
+}
